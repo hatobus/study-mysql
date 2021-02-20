5276e588e4d052fd9cb086af9feed25007502ca6
hatobus <s1240056@gmail.com>
2021-02-20 14:30:53 +0900

    database util

--- database/util.go
+++ database/util.go
@@ -1,1 +1,20 @@
 package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/hatobus/study-mysql/database/config"
+)
+
+func ConnectDB(conf config.MySQLConfig) (*sql.DB, error) {
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?parseTime=true&loc=Asia%%2FTokyo",
+		conf.User,
+		conf.Password,
+		conf.Host,
+		conf.Port,
+		conf.DataBase,
+	)
+
+	return sql.Open("mysql", dsn)
+}
